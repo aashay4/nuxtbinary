4415864ef8df1dd45bbc0932586d4d3497745594
Aashay <aashay.trivedi1@gmail.com>
2020-08-15 22:18:50 +0530

    first

--- pages/binary-converter/index.vue
+++ pages/binary-converter/index.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <div class="w3-main" style="margin-left:250px"><br><br>
+    <div class="w3-main" style="margin-left:250px">
+      <Adsense
+    class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-6829148792481216"
+    data-ad-slot="1665700659"
+    data-ad-format="auto"
+    data-full-width-responsive="true">
+</Adsense><br>
       <div class="w3-row w3-padding-64">
         <div class="w3-twothird w3-container">
           <h1 class="w3-text-teal">Binary Converter</h1>
+          <Adsense
+        class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6829148792481216"
+        data-ad-slot="1416243045"
+        data-ad-format="auto"
+        data-full-width-responsive="true">
+        </Adsense><br><br>
           <p>Full Binary converter, Enter a binary value and click on the button. You will get from binary to hex, dec, oct and text values.</p>
             <div>
             <binarytranslator />
