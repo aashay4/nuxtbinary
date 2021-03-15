7ba4b200aa3f112f7cfc0d7d9ee66d535d7347ff
Aashay <aashay.trivedi1@gmail.com>
2021-03-15 23:18:40 +0530

    first

--- pages/index.vue
+++ pages/index.vue
@@ -12,17 +12,17 @@
       <div class="w3-row w3-padding-64">
         <div class="w3-twothird w3-container">
           <h1 class="w3-text-teal">Binary To Text Converter (Translator)</h1><br>
+          <Adsense
+          class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-6829148792481216"
+          data-ad-slot="1416243045"
+          data-ad-format="auto"
+          data-full-width-responsive="true">
+        </Adsense><br>
           <p>To use the binary to text tool, enter a binary number into the box, click on the binary translator button and get the equivalent text into the output. For example, insert “01000011 01101111 01101110 01110110 01100101 01110010 01110100” into the box and click on the binary to text button, you will get the text string “Convert”.
 If you want to convert text to binary, then enter any text into the text box and click on the button “Text to binary”. You will get the equivalent binary value with our binary translator.<br><br>
 </p>
-<Adsense
-class="adsbygoogle"
-style="display:block"
-data-ad-client="ca-pub-6829148792481216"
-data-ad-slot="1416243045"
-data-ad-format="auto"
-data-full-width-responsive="true">
-</Adsense>
 <div class="w3-hover-shadow w3-panel w3-leftbar w3-light-grey" style="height: 376px;"><br>
           <input type="text" v-model="text_value" style="width: 100%" class="w3-border w3-padding-large w3-padding-32 w3-center" placeholder="Enter a value"/><br><br>
           <button v-on:click="binarytotext()" class="w3-button w3-border w3-teal w3-padding-large w3-hover-gray" style="display: inline-block">Binary to Text</button>
