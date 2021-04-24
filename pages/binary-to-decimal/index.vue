ffaf5823eb172d9687c5a4c938f555489ede78b7
Aashay <aashay.trivedi1@gmail.com>
2021-04-24 16:38:35 +0530

    first

--- pages/binary-to-decimal/index.vue
+++ pages/binary-to-decimal/index.vue
@@ -4,7 +4,7 @@
       <div class="w3-row w3-padding-64">
         <div class="w3-twothird w3-container">
           <h1 class="w3-text-teal">Convert Binary to Decimal</h1>
-          <div>
+          <div v-show="elementVisible">
           <Adsense
         class="adsbygoogle infeed"
         style="display:block"
@@ -201,7 +201,7 @@ export default {
     }
   },
   created() {
-            setTimeout(() => this.elementVisible = true, 2000)
+            setTimeout(() => { this.elementVisible = true }, 2000)
         }
 }
 </script>
