4301d18ed148dd827692dff304a261570ce7f8ac
Aashay <aashay.trivedi1@gmail.com>
2021-04-24 15:11:59 +0530

    first

--- pages/index.vue
+++ pages/index.vue
@@ -257,65 +257,6 @@ export default {
     'binarytranslator': binarytranslator,
     'binarytext': binarytext
   },
-  data () {
-    return {
-      text_value: null,
-            ans: "",
-            aaa: null,
-            livePaper: false
-    }
-  },
-  methods: {
-    texttobinary() {
-
-            for (var i=0; i < this.text_value.length; i++)
-            {
-              var e = this.text_value[i].charCodeAt(0);
-              var s = "";
-              do {
-                var a = e % 2;
-                e = (e - a) / 2;
-        s = a + s;
-      }while(e!=0);
-        while(s.length<8) {
-          if(s.length === 7) {
-            s = " 0" + s;
-            this.ans += s;
-          }
-          if(s.length === 6) {
-          s = " 00" + s;
-          this.ans += s;
-          }
-          if(s.length === 5) {
-          s = " 000" + s;
-          this.ans += s;
-          }
-          if(s.length === 4) {
-          s = " 0000" + s;
-          this.ans += s;
-          }
-          if(s.length === 3) {
-          s = " 00000" + s;
-          this.ans += s;
-          }
-        }
-              }
-            },
-    binarytotext() {
-        var bintext, textresult="", binlength;
-        this.aaa = this.text_value;
-        bintext = this.aaa.replace(/[^01]/g, "");
-			binlength = bintext.length-(bintext.length%8);
-			for(var z=0; z<binlength; z=z+8) {
-				textresult += String.fromCharCode(parseInt(bintext.substr(z,8),2));
-                                this.ans = textresult;
-        }
-      },
-   reset() {
-     this.ans = '',
-     this.text_value= null
-   },
-  }
 }
 </script>
 
